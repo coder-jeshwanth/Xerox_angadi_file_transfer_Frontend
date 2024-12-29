9e515ae5ee43cf06abba0fe13c8a377b0884a40c
coder-jeshwanth <annamalajeshwanth@gmail.com>
2024-12-29 15:09:01 +0530

    not a finalllllllllll

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/auth';
+const API_BASE_URL = 'https://xerox.likithprojects.live/api/auth';
 
 export const login = async (username, password) => {
     try {
